03860c25e43d3e0b165018da4d389541a93c56a2
vignesh.kc <vignesh.kc@zohocorp.com>
2025-01-18 00:05:01 +0530

    resume changes

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
+import CV from 'react-cv';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <CV personalData={{
+        name: 'Vignesh K',
+        title: 'Front-end Developer',
+        image: 'https://iamvignesh.dev/_next/image?url=%2Fme.jpg&w=256&q=75',
+        contacts: [
+          { type: 'email', value: 'official2vignesh@gmail.com' },
+            { type: 'phone', value: '+91 9840051399' },
+            { type: 'location', value: 'India' },
+            { type: 'website', value: 'iamvignesh.dev/' },
+            { type: 'linkedin', value: 'linkedin.com/in/vignesh-karunakaran-4730039b/' },
+            { type: 'twitter', value: 'twitter.com/Vk3y3' },
+            { type: 'github', value: 'github.com/vignesh-karunakaran' }
+        ]
+      }}
+      sections= {[{
+        type: 'text',
+        title: 'Career Profile',
+        content: `Experienced Front-end Developer with a demonstrated history of working in the information technology and services industry. Skilled in <b>Javascript</b>, <b>React</b>, <b>Ember</b>, <b>Node</b>, <b>Express</b>, <b>Java</b>, <b>Gulp</b>, <b>HTML</b>, Information Security, and Linux. Strong engineering professional with a Bachelor's Degree focused in Computer Science.`,
+        icon: 'usertie'
+      }]}
+
+      branding={false}
+      />
   );
 }
 
